981be75bdf980d7560e6e6348abebf65c67c480b
Ioannis Mavroukakis <imavroukakis@gmail.com>
2022-12-01 15:16:10 +0200

    Day one solution

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,15 +1,39 @@
+import kotlin.math.max
+
 fun main() {
     fun part1(input: List<String>): Int {
-        return input.size
+        var max = 0
+        var sum = 0
+        input.forEach {
+            if (it.isNotEmpty()) {
+                sum += it.toInt()
+            } else {
+                max = max(sum, max)
+                sum = 0
+            }
+        }
+        return max
     }
 
     fun part2(input: List<String>): Int {
-        return input.size
+        val elfCalories = mutableListOf<Int>()
+        var sum = 0
+        input.forEach {
+            if (it.isNotEmpty()) {
+                sum += it.toInt()
+            } else {
+                elfCalories.add(sum)
+                sum = 0
+            }
+        }
+        elfCalories.add(sum)
+        return elfCalories.sortedDescending().slice(0..2).sum()
     }
 
     // test if implementation meets criteria from the description, like:
     val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
+    check(part1(testInput) == 24000)
+    check(part2(testInput) == 45000)
 
     val input = readInput("Day01")
     println(part1(input))
