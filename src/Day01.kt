80ec98584c2904e999faa7363ff963c29aac9ba5
Ioannis Mavroukakis <imavroukakis@gmail.com>
2022-12-01 19:57:54 +0200

    Day one solution cleanup

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,20 +1,14 @@
 import java.io.File
 
 fun main() {
-    fun elfData(input: String): List<List<Int>> {
-        val data = input.split("\n\n").map { elf ->
-            elf.lines().map { it.toInt() }
-        }
-        return data
+    fun elfData(input: String): List<List<Int>> = input.split("\n\n").map { elf ->
+        elf.lines().map { it.toInt() }
     }
 
-    fun part1(input: String): Int {
-        return elfData(input).maxOf { it.sum() }
-    }
+    fun part1(input: String): Int = elfData(input).maxOf { it.sum() }
 
-    fun part2(input: String): Int {
-        return elfData(input).sortedByDescending { it.sum() }.take(3).sumOf { it.sum() }
-    }
+    fun part2(input: String): Int =
+        elfData(input).sortedByDescending { it.sum() }.take(3).sumOf { it.sum() }
 
     check(part2(File("src/Day01_test.txt").readText()) == 45000)
     check(part1(File("src/Day01_test.txt").readText()) == 24000)
